d9c7aae246c24279a8bb28bc53109eac0acdc6ab
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-09-29 16:17:23 +0200

    09.29. órai gyakorlás funny calendar

--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,135 @@
-const monthComponent = function (nth, name, days){
+const year = [
+    {
+      month: "January",
+      id: "jan",
+      nth: 1,
+      days: 31,
+    },
+    {
+      month: "February",
+      id: "febr",
+      nth: 2,
+      days: 28,
+    },
+    {
+      month: "March",
+      id: "marc",
+      nth: 3,
+      days: 31,
+    },
+    {
+      month: "April",
+      id: "apr",
+      nth: 4,
+      days: 30,
+    },
+    {
+      month: "May",
+      id: "may",
+      nth: 5,
+      days: 31,
+    },
+    {
+      month: "June",
+      id: "june",
+      nth: 6,
+      days: 30,
+    },
+    {
+      month: "July",
+      id: "july",
+      nth: 7,
+      days: 31,
+    },
+    {
+      month: "August",
+      id: "aug",
+      nth: 8,
+      days: 31,
+    },
+    {
+      month: "September",
+      id: "sept",
+      nth: 9,
+      days: 30,
+    },
+    {
+      month: "October",
+      id: "oct",
+      nth: 10,
+      days: 31,
+    },
+    {
+      month: "November",
+      id: "nov",
+      nth: 11,
+      days: 30,
+    },
+    {
+      month: "December",
+      id: "dec",
+      nth: 12,
+      days: 31,
+    },
+  ];
+  
+  const monthComponent = function (nth, name, days) {
     let daysHtml = "";
- 
-     for (let i = 1; i <=days; i++ ){
-         daysHtml += dayComponent(i);
-     }
- 
-     return`
-         <section id="${nth}" class="${name}">
-             <h2>${name}</h2>
-             ${daysHtml}
-         </section>
-     `
- 
- };
- 
- const dayComponent = function(dayCount) {
-     return `
-         <div class="day">${dayCount}</div>    
-     `
- };
- 
- console.log("hello");
- const rootElement = document.querySelector("#root");
- 
- rootElement.insertAdjacentHTML("beforeend", "<button>show calendar</button>")
- 
- const buttonElement = rootElement.querySelector("button");
- buttonElement.addEventListener("click" , function(){
- })
- 
- rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "January" , 31));
- rootElement.insertAdjacentHTML("beforeend", monthComponent(2, "February" , 28));
- rootElement.insertAdjacentHTML("beforeend", monthComponent(20, "Pityuhónap" , 9));
- // rootElement.style.color="blue"; NEM SZABAD CSINÁLNI
+  
+    for (let i = 1; i <= days; i++) {
+      daysHtml += dayComponent(i);
+    }
+  
+    return `
+          <section id="${nth}" class="${name}">
+              <h2>${name}</h2>
+              <div class="days">${daysHtml}<div>
+          </section>
+      `;
+  };
+  
+  const dayComponent = function (dayCount) {
+    return `
+          <div class="day">${dayCount}</div>    
+      `;
+  };
+  
+  console.log("hello");
+  console.log(year[0]);
+  const rootElement = document.querySelector("#root");
+  
+  rootElement.insertAdjacentHTML("beforeend", "<button>Add month</button>");
+  
+  const buttonElement = rootElement.querySelector("button");
+  
+  // let monthIndex = 0;
+  
+  buttonElement.addEventListener("click", function () {
+    // if (monthIndex < 12) {
+    //   rootElement.insertAdjacentHTML(
+    //     "beforeend",
+    //     monthComponent(
+    //       year[monthIndex].nth,
+    //       year[monthIndex].month,
+    //       year[monthIndex].days
+    //     )
+    //   );
+    //   console.log(year[monthIndex]);
+    //   monthIndex++;
+    // } else {
+    //   buttonElement.setAttribute("disabled", "");
+    // }
+  
+      for(let monthIndex=0; monthIndex<12; monthIndex++){
+        rootElement.insertAdjacentHTML(
+              "beforeend",     
+          monthComponent(year[monthIndex].nth, year[monthIndex].month, year[monthIndex].days)
+        )
+      }
+  
+  });
+  
+  // rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "January" , 31));
+  // rootElement.insertAdjacentHTML("beforeend", monthComponent(2, "February" , 28));
+  // rootElement.insertAdjacentHTML("beforeend", monthComponent(20, "Pityuhónap" , 9));
+  // rootElement.style.color="blue"; NEM SZABAD CSINÁLNI
